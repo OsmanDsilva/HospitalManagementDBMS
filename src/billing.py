54d3cfb4c1ad9eea79d791366ad889f022f19f33
paradoxinfinite <10831629+ParadoxInfinite@users.noreply.github.com>
2018-11-23 22:14:09 +0530

    Updated billing.py. Fixed the data retrieval from db. Code works as expected

--- src/billing.py
+++ src/billing.py
@@ -1,6 +1,7 @@
 from tkinter import *
 import sqlite3
 import os.path
+import datetime
 import sys
 sys.path.append(os.path.abspath(os.path.dirname(__file__) + '/' + '../..'))
 from HospitalManagementDBMS import settings
@@ -17,6 +18,11 @@ class Application:
         self.master = master
         self.heading = Label(master, text="Sagar Hospitals",  fg='steelblue',bg='lightgreen', font=('arial 40 bold'))
         self.heading.place(x=150, y=0)
+        now = datetime.datetime.now()
+        date = now.strftime("%d-%m-%Y %H:%M")
+        self.heading = Label(master, text=date, bg='lightgreen')
+        self.heading.place(x=750, y=10)
+
 
         self.name = Label(master, text="Enter Patient's Name",bg='lightgreen', font=('arial 18 bold'))
         self.name.place(x=0, y=60)
@@ -76,24 +82,34 @@ class Application:
         self.total = Label(frameReason, text="Total", font=('arial 18 bold'), fg='black', bg='lightgreen')
         self.total.place(x=0, y=160)
 
-        cCost = "SELECT cost from reason where name = ?"
-        cMedifee="SELECT medicalfee from reason where name = ?"
-        #vCost = c.execute(cCost,(settings.diseases[settings.diseases.index(self.reason)],))
-        self.ent7 = Label(frameReason, text=(settings.diseases[settings.diseases.index(self.reason)]), anchor=W,bg='lightgreen',font=('arial 14'))
+        condition = (settings.diseases[settings.diseases.index(self.reason)])
+        comand1 = "SELECT * FROM reason where name LIKE ?"
+        self.res = c.execute(comand1,(settings.diseases[settings.diseases.index(self.reason)],))
+        for self.row in self.res :
+            varCost = self.row[1]
+            varMedCost = self.row[2]
+        self.ent7 = Label(frameReason, text=condition, anchor=W,bg='lightgreen',font=('arial 14 bold'))
         self.ent7.place(x=300,y=0)
 
-        self.ent8 = Label(frameReason, text=vCost, anchor=W,bg='lightgreen',font=('arial 14 bold'))
+        self.ent8 = Label(frameReason, text=varCost, anchor=W,bg='lightgreen',font=('arial 14 bold'))
         self.ent8.place(x=300,y=40)
 
-        
+        gst = int(varCost*0.14)
+        self.ent9 = Label(frameReason, text=gst, anchor=W,bg='lightgreen',font=('arial 14 bold'))
+        self.ent9.place(x=300,y=80)
+
+        self.ent10 = Label(frameReason, text=varMedCost, anchor=W,bg='lightgreen',font=('arial 14 bold'))
+        self.ent10.place(x=300,y=120)
+
+        self.ent11 = Label(frameReason, text=varCost+gst+varMedCost, anchor=W,bg='lightgreen',font=('arial 14 bold'))
+        self.ent11.place(x=300,y=160)
 
 
+        self.update = Button(self.master, text="Print", width=20, height=2, bg='lightblue')
+        self.update.place(x=300, y=500)
 
-        # self.update = Button(self.master, text="Update", width=20, height=2, bg='lightblue')
-        # self.update.place(x=400, y=420)
-        #
-        # self.delete = Button(self.master, text="Delete", width=20, height=2, bg='red')
-        # self.delete.place(x=150, y=420)
+        self.delete = Button(self.master, text="Save", width=20, height=2, bg='red')
+        self.delete.place(x=0, y=500)
 root = Tk()
 b = Application(root)
 root.geometry("1200x720+0+0")
